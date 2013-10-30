468498fc745feb32cc9df9c6e50ef500fd8b4d26
Jon <jon@sideprojectco.com>
2013-10-30 15:15:26 -0400

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FristApp::Application.config.secret_key_base = '3aed3280f3bff669cdf0c992a9b516b4e7486621e35a924e4c8664a2e11f065396a117fcaacb781da801b5771152f6cb5a36f9e8a43e1204dc26b0e2410b7c98'
